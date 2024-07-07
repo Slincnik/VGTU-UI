24302a23f6aa92b93dd92839e90b43d9b8d994a1
grigoriyburchakov <schuschpan.01@yandex.ru>
2024-07-07 23:46:06 +0300

    build:change eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,27 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
+const path = require('node:path')
+const createAliasSetting = require('@vue/eslint-config-airbnb/createAliasSetting')
 
 module.exports = {
   root: true,
+  env: {
+      node: true,
+  },
   'extends': [
     'plugin:vue/vue3-essential',
-    'eslint:recommended',
+    // 'eslint:recommended',
+    '@vue/eslint-config-airbnb',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  settings: {
+    ...createAliasSetting({
+      '@': `${path.resolve(__dirname, './src')}`
+    })
   }
+
 }
