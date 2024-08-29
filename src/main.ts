cc783355172ae738dfbf4265e19f065d9cc7a47f
Slincnik <pattyshow21@mail.ru>
2024-08-29 18:22:00 +0300

    feat: preparing for keycloak

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ const vuetify = createVuetify({
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(router())
 app.use(vuetify)
 
 app.mount('#app')
--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -1,75 +1,81 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import { loadLayoutMiddleware } from './middlewares/loadLayout'
 import { AppLayoutsEnum } from '@/layouts/layouts.types'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/TestView.vue'),
-      meta: {
-        layout: AppLayoutsEnum.authed
-      }
-    },
-    {
-      path: '/login',
-      name: 'LoginPage',
-      component: () => import('@/views/Auth/LoginPage.vue')
-    },
-    {
-      path: '/schedule',
-      name: 'SchedulePage',
-      component: () => import('@/views/TestView.vue'),
-      meta: {
-        layout: AppLayoutsEnum.authed
-      }
-    },
-    {
-      path: '/surveys',
-      name: 'SurveysPage',
-      component: () => import('@/views/Surveys/SurveysView.vue'),
-      meta: {
-        layout: AppLayoutsEnum.authed
-      }
-    },
-    {
-      path: '/profile',
-      name: 'ProfilePage',
-      component: () => import('@/views/Profile/ProfileView.vue'),
-      meta: {
-        layout: AppLayoutsEnum.authed
-      },
-      children: [
-        {
-          path: 'main',
-          name: 'ProfileMain',
-          component: () => import('@/components/ProfileInfo/ProfileInfo.vue')
-        },
-        {
-          path: 'orders',
-          name: 'ProfileOrders',
-          component: () => import('@/components/ProfileInfo/ProfileInfo.vue')
-        },
-        {
-          path: 'payments',
-          name: 'ProfilePayments',
-          component: () => import('@/components/ProfileInfo/ProfileInfo.vue')
-        }
-      ],
-      redirect: () => {
-        return { name: 'ProfileMain' }
-      }
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'NotFound',
-      component: () => import('@/components/NotFound/NotFound.vue')
+const routes: readonly RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/TestView.vue'),
+    meta: {
+      layout: AppLayoutsEnum.authed
     }
-  ]
-})
+  },
+  {
+    path: '/login',
+    name: 'LoginPage',
+    component: () => import('@/views/Auth/LoginPage.vue')
+  },
+  {
+    path: '/schedule',
+    name: 'SchedulePage',
+    component: () => import('@/views/TestView.vue'),
+    meta: {
+      layout: AppLayoutsEnum.authed
+    }
+  },
+  {
+    path: '/surveys',
+    name: 'SurveysPage',
+    component: () => import('@/views/Surveys/SurveysView.vue'),
+    meta: {
+      layout: AppLayoutsEnum.authed
+    }
+  },
+  {
+    path: '/profile',
+    name: 'ProfilePage',
+    component: () => import('@/views/Profile/ProfileView.vue'),
+    meta: {
+      layout: AppLayoutsEnum.authed
+    },
+    children: [
+      {
+        path: 'main',
+        name: 'ProfileMain',
+        component: () => import('@/components/ProfileInfo/ProfileInfo.vue')
+      },
+      {
+        path: 'orders',
+        name: 'ProfileOrders',
+        component: () => import('@/components/ProfileInfo/ProfileInfo.vue')
+      },
+      {
+        path: 'payments',
+        name: 'ProfilePayments',
+        component: () => import('@/components/ProfileInfo/ProfileInfo.vue')
+      }
+    ],
+    redirect: () => {
+      return { name: 'ProfileMain' }
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import('@/components/NotFound/NotFound.vue')
+  }
+]
 
-router.beforeEach(loadLayoutMiddleware)
+const initRouter = () => {
+  const router = createRouter({
+    history: createWebHistory(),
+    routes
+  })
 
-export default router
+  router.beforeEach(loadLayoutMiddleware)
+
+  return router
+}
+
+export default initRouter
