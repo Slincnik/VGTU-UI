a1db14747ab4886f92e6e19543c84c00dc0d3acc
Slincnik <pattyshow21@mail.ru>
2024-08-31 22:19:44 +0300

    fix: fixing prettier & eslint config, change vue parser in prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,7 @@ module.exports = {
     })
   },
   rules: {
+    'prettier/prettier': 'error',
     'import/no-extraneous-dependencies': 'off',
     'import/no-unresolved': 'off',
     'vue/html-self-closing': [
