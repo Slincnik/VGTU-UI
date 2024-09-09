f642435cd6a84626058176138b822bc3979bb89f
Slincnik <pattyshow21@mail.ru>
2024-09-09 16:28:57 +0300

    build: update gitlab ci file

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,3 @@
 #! /bin/bash
 set -xe
-sudo docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD docker.io
-sudo docker network create -d bridge cchgeu_network || true
-sudo docker rm -f cchgeu-frontend || true
-sudo docker run --rm -d --name cchgeu-frontend -p 80:80 \
-     --network=cchgeu_network \
-     "${CONTAINER_IMAGE_NAME}:latest"
+sudo docker-compose up -d --build
