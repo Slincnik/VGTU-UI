0b4d23a6cae22eda2e35ea5d345d947b2b03c7df
Slincnik <pattyshow21@mail.ru>
2025-05-06 21:00:55 +0300

    Updates

---
+++ assets/survey.base-DTJJ66CG.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index-CoFnn5Wp.js";import{a as n}from"./index-B9cWahII.js";const o=async()=>{const s=await n();return(await e.get(`survey/all?student=${s}`)).data.content},p=async s=>(await e.get(`survey/${s}`)).data,u=async s=>(await e.post("survey/answer",s)).data,c=async s=>(await e.post("survey/answer/finished",s)).data;export{p as a,c as f,o as g,u as s};
