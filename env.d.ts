b1e82ad9ac303d2643094af3efa3518202cf2d36
Slincnik <pattyshow21@mail.ru>
2025-05-06 19:31:05 +0300

    build: preparing for mock

--- env.d.ts
+++ env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_KEYCLOAK_REALM: string
   readonly VITE_KEYCLOAK_CLIENT_ID: string
   readonly VITE_BASE_URL: string
+  readonly VITE_USE_MOCKS: string
 }
 
 interface ImportMeta {
